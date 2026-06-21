fc9c6dc7d2a699c85d5011f54984f1b08a1c6439
Obsidian_Ninja <gurvxnsh.gupta@gmail.com>
2026-06-22 03:59:22 +0530

    add stream

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,56 @@
+import { Inngest } from "inngest";
+import { connectDB } from "./db.js";
+import User from "../models/Users.js";
+import { deleteStreamUser, upsertStreamUser } from "./stream.js";
+
+// Inngest client
+export const inngest = new Inngest({
+  id: "InterviewIQ",
+});
+
+// Sync user when created
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id, email_addresses, first_name, last_name, image_url } = event.data;
+
+    const newUser = {
+      clerkId: id,
+      email: email_addresses?.[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}`.trim(),
+      profile_img: image_url,
+    };
+
+    await User.create(newUser);
+
+    await upsertStreamUser({
+      id:newUser.clerkId.toString(),
+      name: newUser.name,
+      image: newUser.profileImage
+    });
+  }
+);
+
+// Delete user when removed
+const deleteUserFromDb = inngest.createFunction(
+  {
+    id: "delete-user-from-db",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    await connectDB();
+
+    const { id } = event.data;
+
+    await User.deleteOne({ clerkId: id });
+
+    await deleteStreamUser(id.toString());
+  }
+);
+
+export const functions = [syncUser, deleteUserFromDb];
